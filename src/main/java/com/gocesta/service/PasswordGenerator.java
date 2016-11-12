3d5c2e5dfe021c8f8408eca46d538deea8b05954
reddygocesta <narasimhareddy@gocesta.com>
2016-11-12 16:36:22 +0530

    package changed

--- src/main/java/com/gocesta/service/PasswordGenerator.java
+++ src/main/java/com/gocesta/service/PasswordGenerator.java
@@ -4,12 +4,12 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 public class PasswordGenerator {
 
-	public static void main(String[] args) {
+	/*public static void main(String[] args) {
 
 		String password = "steve";
 		BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 		String hashedPassword = passwordEncoder.encode(password);
 		System.out.println("Password : "+hashedPassword);
 
-	}
+	}*/
 }
